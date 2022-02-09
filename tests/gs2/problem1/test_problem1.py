90f0e85593b44010cc9eda1120557614c65d625e
agross03 <abug2284@gmail.com>
2022-02-09 13:35:42 -0600

    AG3. Added a test case.

--- tests/gs2/problem1/test_problem1.py
+++ tests/gs2/problem1/test_problem1.py
@@ -34,7 +34,7 @@ def test_bs_between_ws(solution):
     # print('actual:{0} '.format(actual))
     assert actual == expected
 
-def test_bs_between_ws(solution):
+def test_all_ws(solution):
     """
     ("www","Wendy")
     """
@@ -43,7 +43,7 @@ def test_bs_between_ws(solution):
     # print('actual:{0} '.format(actual))
     assert actual == expected    
 
-def test_bs_between_ws(solution):
+def test_bs_and_ws(solution):
     """
     ("bbbbbwwwwwwbbwwwbwbwww","Wendy")
     """
